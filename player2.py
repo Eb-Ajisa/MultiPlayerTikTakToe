7d7ad22d73750e046b13a5bb73ab40fc25dc2c97
Ebenezer Ajisafe <ebenezeroajisafe@gmail.com>
2025-03-26 01:23:28 -0400

    Update player2.py

--- player2.py
+++ player2.py
@@ -1,5 +1,9 @@
 from socket import *
-
+import tkinter as tk
+import tkinter.scrolledtext
+from tkinter import simpledialog
+import pickle
+import threading
 hostname = gethostname()
 ip_address = gethostbyname(hostname)
 ip_address = str(ip_address)
@@ -8,5 +12,18 @@ serverName = ip_address # server IP
 serverPort = 10000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
+
+game = True
+
+
+while game == True:
+    sentence = input('Press 1 to start game: ')
+    clientSocket.send(sentence.encode())
+    
+    gamestart = clientSocket.recv(4096).decode()
+    exec(gamestart)
+
+    input("Press Enter to continue...")
+
 
 

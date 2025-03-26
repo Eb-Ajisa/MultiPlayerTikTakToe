5867a517b97224f24f5bd876535278f08d82e81e
Ebenezer Ajisafe <ebenezeroajisafe@gmail.com>
2025-03-26 16:37:44 -0400

    Update player2.py

--- player2.py
+++ player2.py
@@ -1,9 +1,13 @@
 from socket import *
-import tkinter as tk
+from time import sleep
+import tkinter
 import tkinter.scrolledtext
+from tkinter import messagebox
 from tkinter import simpledialog
-import pickle
 import threading
+import pickle
+
+#Socket Configuration
 hostname = gethostname()
 ip_address = gethostbyname(hostname)
 ip_address = str(ip_address)
@@ -15,15 +19,37 @@ clientSocket.connect((serverName,serverPort))
 
 game = True
 
+#Execute functions mainly start the mainloop for the gui
+def func(gm):
+    exec(gm)
 
+#Game initalization
+sentence = input('Press 1 to start game: ')
+
+#Error Handling
+if sentence != '1':
+      game = False
+      print("Incorrect value, closing connection")
+      clientSocket.shutdown(SHUT_RDWR)
+      clientSocket.close()
+
+#Game/SocketLoop
 while game == True:
-    sentence = input('Press 1 to start game: ')
-    clientSocket.send(sentence.encode())
-    
-    gamestart = clientSocket.recv(4096).decode()
-    exec(gamestart)
 
-    input("Press Enter to continue...")
+    clientSocket.send(sentence.encode())
+    gm = clientSocket.recv(4096).decode()
+
+    #Send the create_game() function to the thread
+    tiktaktoe = threading.Thread(target=func, args=(gm,))
+    tiktaktoe.start()
+    tiktaktoe.join()
+    again = input("Go again?(Press 1 to go again): ")
+    if again != '1':
+        game = False
+        clientSocket.shutdown(SHUT_RDWR)
+        clientSocket.close()
+        break
+
 
 
 

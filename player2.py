d9b8b0cd748399fa3970a05c9e9621568f333a9a
Ebenezer Ajisafe <ebenezeroajisafe@gmail.com>
2025-03-25 20:29:35 -0400

    Update player2.py

--- player2.py
+++ player2.py
@@ -9,9 +9,4 @@ serverPort = 10000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
-game = True
-
-while game == True:
-    sentence = input('Press 1 to start game: ')
-    clientSocket.send(sentence.encode())
 
